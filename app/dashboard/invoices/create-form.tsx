6b4ca63abc41d34a2d9b21929f93c7b2380338b4
rhmn <rahmansrg77@gmail.com>
2025-12-15 18:51:20 +0700

    Perubahan form action

--- app/dashboard/invoices/create-form.tsx
+++ app/dashboard/invoices/create-form.tsx
@@ -18,7 +18,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
   const [state, formAction] = useActionState(createInvoice, initialState);
 
   return (
-    <form action={createInvoice}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
